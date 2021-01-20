afacfe8c6455e6c4c70e411b36683f437991fa16
Erkan <caliskan.erkaann@gmail.com>
2021-01-20 17:30:27 +0300

    some style files added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    test
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { ref } from 'vue'
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  setup () {
+    const user = ref('Erkan')
+    return {
+      user
+    }
   }
 }
 </script>
